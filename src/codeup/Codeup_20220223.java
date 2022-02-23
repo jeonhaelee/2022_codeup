9cab8ab7234c23c5e6ba96d7c686bd2f3fab93d0
jeonhaelee <jhealee0624@gmail.com>
2022-02-23 19:59:11 +0900

    비트단위로 AND하여 출력하기

--- src/codeup/Codeup_20220223.java
+++ src/codeup/Codeup_20220223.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Codeup_20220223 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		char num2 = (char)num;
-		System.out.println(num2);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println(num1 & num2);
 		sc.close();
 	}
 }
