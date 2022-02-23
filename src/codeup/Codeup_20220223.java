76cfdf294eaa1a3d82b3aa376c7b4a743d2787c1
jeonhaelee <jhealee0624@gmail.com>
2022-02-23 19:51:21 +0900

    영문자 1개 입력받아 10진수로 출력하기

--- src/codeup/Codeup_20220223.java
+++ src/codeup/Codeup_20220223.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class Codeup_20220223 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String num = sc.next();
-		int num2 = Integer.valueOf(num,16);
-		String num3 = Integer.toOctalString(num2);
-		System.out.println(num3);
+		char num = sc.next().charAt(0);
+		int num2 = (int)num;
+		System.out.println(num2);
 		sc.close();
 	}
 }
