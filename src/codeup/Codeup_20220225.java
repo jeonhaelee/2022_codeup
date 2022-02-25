b04f573dda6e950a3e76cfd8a9c38108a6627e84
jeonhaelee <jhealee0624@gmail.com>
2022-02-25 12:29:28 +0900

    당신의 사주를 봐드립니다1

--- src/codeup/Codeup_20220225.java
+++ src/codeup/Codeup_20220225.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Codeup_20220225 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		String aaa = a % 2 == 0 ? "짝수" : "홀수";
-		String bbb = b % 2 == 0 ? "짝수" : "홀수";
-		String ab = (a+b) % 2 == 0 ? "짝수" : "홀수";
-		System.out.printf(aaa + "+" + bbb + "=" + ab);
+		int yyyy = sc.nextInt();
+		int mm = sc.nextInt();
+		int dd = sc.nextInt();
+		if ((yyyy-mm+dd) % 10 ==0) {
+			System.out.println("대박");
+		} else { System.out.println("그럭저럭");}
 	}
 }
