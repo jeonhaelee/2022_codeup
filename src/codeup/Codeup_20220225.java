54596d7cd69968d4b138ad3546566faf33ccf8a7
jeonhaelee <jhealee0624@gmail.com>
2022-02-25 12:32:49 +0900

    당신의 사주를 봐드립니다2

--- src/codeup/Codeup_20220225.java
+++ src/codeup/Codeup_20220225.java
@@ -8,7 +8,8 @@ public class Codeup_20220225 {
 		int yyyy = sc.nextInt();
 		int mm = sc.nextInt();
 		int dd = sc.nextInt();
-		if ((yyyy-mm+dd) % 10 ==0) {
+		int num = (int)((yyyy+mm+dd) / 100) ;
+		if (num % 2 ==0) {
 			System.out.println("대박");
 		} else { System.out.println("그럭저럭");}
 	}
