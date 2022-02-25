8c0905e3379c000849223ef65e16c0349b4bd221
jeonhaelee <jhealee0624@gmail.com>
2022-02-25 12:13:24 +0900

    특별한 공던지기3

--- src/codeup/Codeup_20220225.java
+++ src/codeup/Codeup_20220225.java
@@ -1,5 +1,13 @@
 package codeup;
 
-public class Codeup_20220225 {
+import java.util.Scanner;
 
+public class Codeup_20220225 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if (a>=50 && a<=70 || a % 6 == 0) {
+			System.out.println("win");
+		} else { System.out.println("lose");}
+	}
 }
