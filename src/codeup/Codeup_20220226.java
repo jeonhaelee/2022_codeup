f6bf5c219f81afc0b054cef052ca00e3cb5e22ce
jeonhaelee <jhealee0624@gmail.com>
2022-02-26 17:40:21 +0900

    윤년 판별

--- src/codeup/Codeup_20220226.java
+++ src/codeup/Codeup_20220226.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class Codeup_20220226 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);      
-	    int time = sc.nextInt();
-	    int score = sc.nextInt();
-	    int get = 0;
-	    for (int i = time; i < 90; i+=5){
-	      get += 1;
-	    }
-	    System.out.println(score+get);
-	    sc.close();
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+		sc.close();
 	}
 }
