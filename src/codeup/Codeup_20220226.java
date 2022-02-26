d7681c8a1fafcb1fe51371516f98874ce61b592b
jeonhaelee <jhealee0624@gmail.com>
2022-02-26 17:42:42 +0900

    나이 계산1

--- src/codeup/Codeup_20220226.java
+++ src/codeup/Codeup_20220226.java
@@ -1,18 +1,21 @@
 package codeup;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Codeup_20220226 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
+		a = (int) (a / 10000);
 		int b = sc.nextInt();
-		int c = sc.nextInt();
+		int year = 0;
+		if (b == 1 || b == 2) {
+			year = 1900 + a;
+		} else if (b == 3 || b == 4) {
+			year = 2000 + a;
+		}
+		System.out.println(2012 - year + 1);
 
-		int[] number = { a, b, c };
-		Arrays.sort(number);
-		System.out.println(number[1]);
 		sc.close();
 	}
 }
