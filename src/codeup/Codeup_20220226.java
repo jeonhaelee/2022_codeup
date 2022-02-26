85a61ab42bdd7b4feb1c4e5bb6a79c47f27bf226
jeonhaelee <jhealee0624@gmail.com>
2022-02-26 17:41:42 +0900

    두번째 수

--- src/codeup/Codeup_20220226.java
+++ src/codeup/Codeup_20220226.java
@@ -1,16 +1,18 @@
 package codeup;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Codeup_20220226 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt();
-		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-			System.out.println("yes");
-		} else {
-			System.out.println("no");
-		}
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int[] number = { a, b, c };
+		Arrays.sort(number);
+		System.out.println(number[1]);
 		sc.close();
 	}
 }
