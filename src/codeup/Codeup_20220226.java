69eb6a3c74d64405545a146324b20d5d02793a05
jeonhaelee <jhealee0624@gmail.com>
2022-02-26 17:39:17 +0900

    축구의 신1

--- src/codeup/Codeup_20220226.java
+++ src/codeup/Codeup_20220226.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class Codeup_20220226 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		if (a > 170 && b > 170 && c > 170) {
-			System.out.println("PASS");
-		} else {
-			System.out.println("CRASH");
-		}
-		sc.close();
+		Scanner sc = new Scanner(System.in);      
+	    int time = sc.nextInt();
+	    int score = sc.nextInt();
+	    int get = 0;
+	    for (int i = time; i < 90; i+=5){
+	      get += 1;
+	    }
+	    System.out.println(score+get);
+	    sc.close();
 	}
 }
