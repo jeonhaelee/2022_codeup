53c9fe25315be788754930a1132db84d68e18fc7
jeonhaelee <jhealee0624@gmail.com>
2022-02-27 14:21:36 +0900

    당신의 학번은? 1

--- src/codeup/Codeup_20220227.java
+++ src/codeup/Codeup_20220227.java
@@ -5,18 +5,11 @@ import java.util.Scanner;
 public class Codeup_20220227 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int age = sc.nextInt(); // 나이 입력받기
-		String year = String.valueOf(2012-age+1); // 출생년도를 구해 문자열로 형변환하기
-		String[] year_arr = year.split(""); // 출생년도를 쪼개어 문자열 배열에 넣어주기
-		int b = 0; // 출생년도 앞자리 구분하기
-		if (Integer.parseInt(year_arr[0]) == 1) {
-			b = 1; // 1900년대 출생이면 1
-		} else {
-			b = 3; // 2000년대 출생이면 3
-		}
-		String a; // 출생년도 뒤 두자리만 구해서 a에 넣기
-		a = year_arr[2] + year_arr[3];
-		int a_int = Integer.parseInt(a); // 정수형으로 바꿔주기
-		System.out.printf("%d %d",a_int,b); // 출력
+		int a = sc.nextInt(); // 학년 입력받기
+		int b = sc.nextInt(); // 반 입력받기
+		int c = sc.nextInt(); // 번호 입력받기
+		System.out.printf("%d%d%02d",a,b,c); // 출력
+		// %02d는 일의 자리만 있을 경우 앞을 0으로 채워서 정수를 두자리수로 맞춘다.
+		// 예) 3일 경우, 03으로..
 	}
 }
