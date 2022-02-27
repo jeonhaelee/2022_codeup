25116379cbcac179b1779f8e28d3fa793c545443
jeonhaelee <jhealee0624@gmail.com>
2022-02-27 14:59:54 +0900

    30분 전

--- src/codeup/Codeup_20220227.java
+++ src/codeup/Codeup_20220227.java
@@ -1,18 +1,25 @@
 package codeup;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Codeup_20220227 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt(); // 세 수 입력받기
-		int b = sc.nextInt(); 
-		int c = sc.nextInt(); 
-		int[] numbers = {a,b,c}; // 세 수를 배열에 담아 오름차순으로 정렬하기
-		Arrays.sort(numbers); 
-		for (int i=0; i<numbers.length; i++) { 
-			System.out.printf("%d ",numbers[i]); // 정렬된 순서대로 출력
+		
+		int hour = sc.nextInt(); // 시간과 분 입력받기
+		int minute = sc.nextInt(); 
+		
+		if (minute < 30) { 		
+			if (hour == 0) {
+				hour = 23;
+			} else {
+				hour -= 1;
+			}
+			minute += 30;
+		} else {
+			minute -= 30;
 		}
+		
+		System.out.println(hour + " " + minute); // 출력
 	}
 }
