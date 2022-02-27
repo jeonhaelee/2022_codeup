ef1cc9ab3c831a816e6ea9a8c2109089771ffef1
jeonhaelee <jhealee0624@gmail.com>
2022-02-27 14:23:04 +0900

    당신의 학번은? 2

--- src/codeup/Codeup_20220227.java
+++ src/codeup/Codeup_20220227.java
@@ -8,8 +8,6 @@ public class Codeup_20220227 {
 		int a = sc.nextInt(); // 학년 입력받기
 		int b = sc.nextInt(); // 반 입력받기
 		int c = sc.nextInt(); // 번호 입력받기
-		System.out.printf("%d%d%02d",a,b,c); // 출력
-		// %02d는 일의 자리만 있을 경우 앞을 0으로 채워서 정수를 두자리수로 맞춘다.
-		// 예) 3일 경우, 03으로..
+		System.out.printf("%d%02d%03d",a,b,c); // 출력
 	}
 }
