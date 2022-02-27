5aa3bd658df10963ceefd7f6076c3d7070510941
jeonhaelee <jhealee0624@gmail.com>
2022-02-27 14:35:12 +0900

    세 수 정렬하기

--- src/codeup/Codeup_20220227.java
+++ src/codeup/Codeup_20220227.java
@@ -1,13 +1,18 @@
 package codeup;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Codeup_20220227 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt(); // 학년 입력받기
-		int b = sc.nextInt(); // 반 입력받기
-		int c = sc.nextInt(); // 번호 입력받기
-		System.out.printf("%d%02d%03d",a,b,c); // 출력
+		int a = sc.nextInt(); // 세 수 입력받기
+		int b = sc.nextInt(); 
+		int c = sc.nextInt(); 
+		int[] numbers = {a,b,c}; // 세 수를 배열에 담아 오름차순으로 정렬하기
+		Arrays.sort(numbers); 
+		for (int i=0; i<numbers.length; i++) { 
+			System.out.printf("%d ",numbers[i]); // 정렬된 순서대로 출력
+		}
 	}
 }
