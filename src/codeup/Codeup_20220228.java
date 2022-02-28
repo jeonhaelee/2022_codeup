2040db282a70d9dfd3b765fc10de35616917b093
jeonhaelee <jhealee0624@gmail.com>
2022-02-28 23:44:08 +0900

    정수 1개 입력받아 카운트다운 출력하기 2

--- src/codeup/Codeup_20220228.java
+++ src/codeup/Codeup_20220228.java
@@ -6,7 +6,7 @@ public class Codeup_20220228 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		for (int i = number; i >=1 ; i--) {
+		for (int i = number-1; i >=0 ; i--) {
 			System.out.println(i);
 		}
 	}
