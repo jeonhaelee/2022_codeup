ec8aaace13cce04ec344cd26a90a9722200e89b8
jeonhaelee <jhealee0624@gmail.com>
2022-02-28 23:50:01 +0900

    짝수 합 구하기

--- src/codeup/Codeup_20220228.java
+++ src/codeup/Codeup_20220228.java
@@ -6,8 +6,10 @@ public class Codeup_20220228 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		for (int i = 0; i <= number ; i++) {
-			System.out.println(i);
+		int sum = 0;
+		for (int i = 0; i <= number ; i += 2) {
+			sum += i;
 		}
+		System.out.println(sum);
 	}
 }
