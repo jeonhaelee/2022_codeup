4c04d33ba4280ef380fa8208dacd45f067553721
jeonhaelee <jhealee0624@gmail.com>
2022-02-28 23:42:43 +0900

    정수 1개 입력받아 카운트다운 출력하기 1

--- src/codeup/Codeup_20220228.java
+++ src/codeup/Codeup_20220228.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class Codeup_20220228 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while(true) {
-			int number = sc.nextInt();
-			if (number==0) {
-				break;
-			}
-			System.out.println(number);
+		int number = sc.nextInt();
+		for (int i = number; i >=1 ; i--) {
+			System.out.println(i);
 		}
 	}
 }
