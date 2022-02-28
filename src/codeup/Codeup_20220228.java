caa6a661e4d833f4f3fccb40d56fccba2d285172
jeonhaelee <jhealee0624@gmail.com>
2022-02-28 23:48:12 +0900

    정수 1개 입력받아 그 수까지 출력하기

--- src/codeup/Codeup_20220228.java
+++ src/codeup/Codeup_20220228.java
@@ -6,7 +6,7 @@ public class Codeup_20220228 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		for (int i = number-1; i >=0 ; i--) {
+		for (int i = 0; i <= number ; i++) {
 			System.out.println(i);
 		}
 	}
