80348cd3ea6548b45db1e8fb40aabf099216b94e
jeonhaelee <jhealee0624@gmail.com>
2022-03-01 14:06:30 +0900

    언제까지 더해야 할까?

--- src/codeup_3/Codeup_20220301.java
+++ src/codeup_3/Codeup_20220301.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Codeup_20220301 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while (true) {
-			String word = sc.next();
-			System.out.println(word);
-			if(word.equals("q")) {
+		int number = sc.nextInt();
+		int sum = 0;
+		for (int i=1; i<=number;i++) {
+			sum += i;
+			if(sum >= number) {
+				System.out.println(i);
 				break;
 			}
 		}
