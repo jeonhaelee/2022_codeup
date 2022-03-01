d1573bee648b441c1744eb1fc2de678761b7a33b
jeonhaelee <jhealee0624@gmail.com>
2022-03-01 14:02:43 +0900

    원하는 문자가 입력될 때까지 반복 출력하기

--- src/codeup_3/Codeup_20220301.java
+++ src/codeup_3/Codeup_20220301.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Codeup_20220301 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int num = sc.nextInt();
-		System.out.println(num);
-		while(num != 0) {
-			int number = sc.nextInt();
-			System.out.println(number);
-			num--;
+		while (true) {
+			String word = sc.next();
+			System.out.println(word);
+			if(word.equals("q")) {
+				break;
+			}
 		}
 	}
 }
