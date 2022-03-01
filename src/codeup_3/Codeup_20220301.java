00f41627b1b6bda7be65efb219e7a0abd0cb15d2
jeonhaelee <jhealee0624@gmail.com>
2022-03-01 14:20:03 +0900

    1부터 100까지 출력하기

--- src/codeup_3/Codeup_20220301.java
+++ src/codeup_3/Codeup_20220301.java
@@ -1,18 +1,9 @@
 package codeup_3;
 
-import java.util.Scanner;
-
 public class Codeup_20220301 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		int sum = 0;
-		for (int i=1; i<=number;i++) {
-			sum += i;
-			if(sum >= number) {
-				System.out.println(i);
-				break;
-			}
+		for (int i=1; i<=100;i++) {
+		System.out.printf("%d ",i);
 		}
 	}
 }
