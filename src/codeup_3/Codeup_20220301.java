86d7b7f33ce27f76156f8867ae0b3ff3c0840a45
jeonhaelee <jhealee0624@gmail.com>
2022-03-01 14:22:07 +0900

    1부터 n까지 출력하기

--- src/codeup_3/Codeup_20220301.java
+++ src/codeup_3/Codeup_20220301.java
@@ -1,8 +1,12 @@
 package codeup_3;
 
+import java.util.Scanner;
+
 public class Codeup_20220301 {
 	public static void main(String[] args) {
-		for (int i=1; i<=100;i++) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		for (int i=1; i<=number;i++) {
 		System.out.printf("%d ",i);
 		}
 	}
