a246900325f0d843322227adce8579e247a4df56
jeonhaelee <jhealee0624@gmail.com>
2022-03-02 23:15:43 +0900

    1부터 n까지 합 구하기

--- src/codeup_3/Codeup_20220302.java
+++ src/codeup_3/Codeup_20220302.java
@@ -6,11 +6,10 @@ public class Codeup_20220302 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int b = sc.nextInt();
-		for(int i = a; i<=b ; i++) {
-			if( i % 2 != 0) {
-				System.out.printf("%d ",i);
-			}
+		int sum = 0;
+		for(int i=1; i<=a; i++) {
+			sum += i;
 		}
+		System.out.println(sum);
 	}
 }
