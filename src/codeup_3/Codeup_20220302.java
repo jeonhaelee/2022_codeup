b80e467d598c2226620bea6386068326a709947a
jeonhaelee <jhealee0624@gmail.com>
2022-03-02 23:18:57 +0900

    3의 배수의 합

--- src/codeup_3/Codeup_20220302.java
+++ src/codeup_3/Codeup_20220302.java
@@ -6,9 +6,10 @@ public class Codeup_20220302 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
+		int b = sc.nextInt();
 		int sum = 0;
-		for(int i=1; i<=a; i++) {
-			if(i%2==0) {
+		for(int i=a; i<=b; i++) {
+			if(i%3==0) {
 				sum += i;
 			}
 		}
