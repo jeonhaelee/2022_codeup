7c826ea763930a0d9f757a368861f3d6f862e9b2
jeonhaelee <jhealee0624@gmail.com>
2022-03-03 14:37:26 +0900

    수 나열하기 3

--- src/codeup_3/Codeup_20220303.java
+++ src/codeup_3/Codeup_20220303.java
@@ -6,9 +6,13 @@ public class Codeup_20220303 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int r = sc.nextInt();
+		int m = sc.nextInt();
+		int d = sc.nextInt();
 		int n = sc.nextInt();
-		long num = (long)(a*Math.pow(r, n-1));
+		long num = a;
+		for(int i=1; i<n;i++) {
+			num = (num * m + d);
+		}
 		System.out.println(num);
 	}
 }
