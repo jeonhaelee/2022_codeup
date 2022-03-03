b598fff92a90c23a403411192df7cb2783e986c5
jeonhaelee <jhealee0624@gmail.com>
2022-03-03 14:23:48 +0900

    수 나열하기 2

--- src/codeup_3/Codeup_20220303.java
+++ src/codeup_3/Codeup_20220303.java
@@ -6,9 +6,9 @@ public class Codeup_20220303 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int d = sc.nextInt();
+		int r = sc.nextInt();
 		int n = sc.nextInt();
-		int num = a + (n-1)*d;
+		long num = (long)(a*Math.pow(r, n-1));
 		System.out.println(num);
 	}
 }
