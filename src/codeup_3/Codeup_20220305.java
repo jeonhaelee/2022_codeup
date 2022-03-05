55d5eaa1c2e15b289e3d216f87e7d3a8b5a9a92e
jeonhaelee <jhealee0624@gmail.com>
2022-03-05 23:51:57 +0900

    최댓값, 최솟값

--- src/codeup_3/Codeup_20220305.java
+++ src/codeup_3/Codeup_20220305.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class Codeup_20220305 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		String star = "";
-		String stars = "";
-		for (int j = 1; j <= num; j++) {
-			star += "*";
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int[] numbers = {a,b,c,d,e};
+		int max_num = numbers[0];
+		int min_num = numbers[0];
+		for(int i=1 ; i<numbers.length; i++) {
+			if(Math.max(max_num, numbers[i])==numbers[i]) {
+				max_num = numbers[i];
+			} 
 		}
-		for(int i=1; i<=9;i++) {
-			stars += star;
-			System.out.println(stars);
+		for(int i=1 ; i<numbers.length; i++) {
+			if(Math.min(min_num, numbers[i])==numbers[i]) {
+				min_num = numbers[i];
+			} 
 		}
+		System.out.println(max_num);
+		System.out.println(min_num);
 	}
 }
