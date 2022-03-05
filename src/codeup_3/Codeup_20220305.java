a91c8bfc83be41e95a5688524845999a279c5363
jeonhaelee <jhealee0624@gmail.com>
2022-03-05 23:36:19 +0900

    구구단을 *로 출력하기

--- src/codeup_3/Codeup_20220305.java
+++ src/codeup_3/Codeup_20220305.java
@@ -6,8 +6,14 @@ public class Codeup_20220305 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		for (int i = 1; i <= 9; i++) {
-			System.out.printf("%d*%d=%d\n", num, i, num * i);
+		String star = "";
+		String stars = "";
+		for (int j = 1; j <= num; j++) {
+			star += "*";
+		}
+		for(int i=1; i<=9;i++) {
+			stars += star;
+			System.out.println(stars);
 		}
 	}
 }
