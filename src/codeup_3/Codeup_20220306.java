bc9b94c1c2a87f6b5090ab2d8b32ebee4ee8dec7
jeonhaelee <jhealee0624@gmail.com>
2022-03-06 23:48:35 +0900

    함수로 정수1 리턴하기

--- src/codeup_3/Codeup_20220306.java
+++ src/codeup_3/Codeup_20220306.java
@@ -3,9 +3,10 @@ package codeup_3;
 public class Codeup_20220306 {
 	public static void main(String[] args) {
 		f();
+		System.out.printf("%d",f());
 	}
 
-	private static void f() {
-		System.out.println("A");
+	private static int f() {
+		return 1;
 	}
 }
