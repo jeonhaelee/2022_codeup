4a643ba45727d306edf30ebb0e4976a3a05ed01f
jeonhaelee <jhealee0624@gmail.com>
2022-03-07 17:18:31 +0900

    영어 서수로 표현하기

--- src/codeup_3/Codeup_20220307.java
+++ src/codeup_3/Codeup_20220307.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Codeup_20220307 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int BMI = sc.nextInt();
-		if(BMI<=10) {
-			System.out.println("정상");
-		}else if(BMI<=20) {
-			System.out.println("과체중");
-		}else {
-			System.out.println("비만");
+		int num = sc.nextInt();
+		String word = "th";
+		if(num%10==1) {
+			word = "st";
+		}else if(num%10==2) {
+			word = "nd";
+		}else if(num%10==3) {
+			word = "rd";
 		}
+		if(num==11 || num==12 || num==13) {
+			word = "th";
+		}
+		System.out.println(num+word);
 	}
 }
