5cbaff7a9f41e68f68697132fc56f0d00100e742
jeonhaelee <jhealee0624@gmail.com>
2022-03-07 17:01:10 +0900

    등급 판정

--- src/codeup_3/Codeup_20220307.java
+++ src/codeup_3/Codeup_20220307.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Codeup_20220307 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		if(number>0) {
-			System.out.println("양수");
-		}else if(number<0) {
-			System.out.println("음수");
+		int score = sc.nextInt();
+		if(score>=90) {
+			System.out.println("A");
+		}else if(score>=80) {
+			System.out.println("B");
+		}else if(score>=70) {
+			System.out.println("C");
+		}else if(score>=60) {
+			System.out.println("D");
 		}else {
-			System.out.println("0");
+			System.out.println("F");
 		}
 	}
 }
