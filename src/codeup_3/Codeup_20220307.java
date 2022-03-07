9506c6405022561b5715af1efbc8a8c4015a34f9
jeonhaelee <jhealee0624@gmail.com>
2022-03-07 16:57:48 +0900

    정수 판별

--- src/codeup_3/Codeup_20220307.java
+++ src/codeup_3/Codeup_20220307.java
@@ -6,19 +6,12 @@ public class Codeup_20220307 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		int num_10;
-		int num_1;
-		num_10 = (int) number / 10;
-		num_1 = number % 10;
-		int number_fin = (num_1 * 10 + num_10)*2;
-		if(number_fin/100 >= 0) {
-			number_fin = number_fin%100;
-		}
-		System.out.println(number_fin);
-		if(number_fin<=50) {
-			System.out.println("GOOD");
-		} else {
-			System.out.println("OH MY GOD");
+		if(number>0) {
+			System.out.println("양수");
+		}else if(number<0) {
+			System.out.println("음수");
+		}else {
+			System.out.println("0");
 		}
 	}
 }
