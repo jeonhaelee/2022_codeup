306947238c9a9b0d250d4bbee3f08edabf978227
jeonhaelee <jhealee0624@gmail.com>
2022-03-09 22:52:05 +0900

    축구의 신 2

--- src/codeup_3/Codeup_20220309.java
+++ src/codeup_3/Codeup_20220309.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Codeup_20220309 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		if(a+b <= c) {
-			System.out.println("삼각형아님");
-		} else if (a==b && b==c){
-			System.out.println("정삼각형");
-		}else if(a==b || b==c || c==a) {
-			System.out.println("이등변삼각형");
-		} else if (Math.pow(a, 2)+Math.pow(b, 2)==Math.pow(c, 2)){
-			System.out.println("직각삼각형");
-		} else if (a+b > c){
-			System.out.println("삼각형");
-		} 
+		int time = sc.nextInt();
+		int score_1 = sc.nextInt();
+		int score_2 = sc.nextInt();
 		
+		for(int i=time; i<90;i += 5) {
+				score_1++;
+		}
+		
+		if(score_1>score_2) {
+			System.out.println("win");
+		}else if(score_1<score_2) {
+			System.out.println("lose");
+		}else {
+			System.out.println("same");
+		}
 	}
 }
