98eee5fb4ee5a85371a92a5a46efead7f5af2d3d
jeonhaelee <jhealee0624@gmail.com>
2022-03-09 22:58:27 +0900

    분수 크기 비교

--- src/codeup_3/Codeup_20220309.java
+++ src/codeup_3/Codeup_20220309.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class Codeup_20220309 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int time = sc.nextInt();
-		int score_1 = sc.nextInt();
-		int score_2 = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
 		
-		for(int i=time; i<90;i += 5) {
-				score_1++;
-		}
-		
-		if(score_1>score_2) {
-			System.out.println("win");
-		}else if(score_1<score_2) {
-			System.out.println("lose");
+		if((double)a/b > (double)c/d) {
+			System.out.println(">");
+		}else if((double)a/b < (double)c/d) {
+			System.out.println("<");
 		}else {
-			System.out.println("same");
+			System.out.println("=");
 		}
+	
 	}
 }
