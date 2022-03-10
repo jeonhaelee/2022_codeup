b7c7069de916cc2b4a691a7f38b74110fd2aa758
jeonhaelee <jhealee0624@gmail.com>
2022-03-10 21:37:42 +0900

    0 입력될 때까지 무한 출력하기 2

---
+++ src/codeup_3/Codeup_20220310.java
@@ -0,0 +1,17 @@
+package codeup_3;
+
+import java.util.Scanner;
+
+public class Codeup_20220310 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int num = sc.nextInt();
+			if(num==0) {
+				break;
+			}
+			System.out.println(num);
+			
+		}
+	}
+}
