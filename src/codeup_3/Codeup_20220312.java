856c2978219e3dc7edd043d58857d97343a6aa63
jeonhaelee <jhealee0624@gmail.com>
2022-03-12 23:33:35 +0900

    자릿수 계산

--- src/codeup_3/Codeup_20220312.java
+++ src/codeup_3/Codeup_20220312.java
@@ -6,10 +6,9 @@ public class Codeup_20220312 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int answer = 1;
-		for ( int i=num; i>=1; i--) {
-			answer = answer * i;
-		}
-		System.out.printf("%d",answer);
+		
+		String number = Integer.toString(num);
+		
+		System.out.println(number.length());
 	}
 }
