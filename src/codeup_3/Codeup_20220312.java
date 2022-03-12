dfa4b3ae85caa5beb06ec88c4f327fcae2b565c3
jeonhaelee <jhealee0624@gmail.com>
2022-03-12 23:45:01 +0900

    홀수는 더하고 짝수는 빼고 2

--- src/codeup_3/Codeup_20220312.java
+++ src/codeup_3/Codeup_20220312.java
@@ -7,15 +7,20 @@ public class Codeup_20220312 {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
+		String answer = "";
 		int sum = 0;
 		for(int i=a; i<=b; i++) {
 			if(i%2==0) {
 				sum -= i;
+				answer += "-";
+				answer += i;
 			}
 			else {
 				sum += i;
+				answer += "+";
+				answer += i;
 			}
 		}
-		System.out.println(sum);
+		System.out.println(answer+"="+sum);
 	}
 }
