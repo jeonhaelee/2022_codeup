e8f8c1a740036c92bd9093a0ab8022d48fad95d7
jeonhaelee <jhealee0624@gmail.com>
2022-03-13 18:17:34 +0900

    암호 해독

--- src/codeup_3/Codeup_20220313.java
+++ src/codeup_3/Codeup_20220313.java
@@ -5,25 +5,35 @@ import java.util.Scanner;
 public class Codeup_20220313 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int[] wave = new int[b];
-		for(int i=0; i<b ;i++) {
-			wave[i] = sc.nextInt();
+		int num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			if(prime_count(num)==0) {
+			System.out.println("wrong number");
+			break;
+			}
+			if(num%i==0) {
+				if(prime_count(i)==0) {
+					if(prime_count(num/i)==0) {
+						System.out.println(i + " " + num/i);
+						break;
+					}
+				}else {
+					System.out.println("wrong number");
+					break;
+				}
+			}
 		}
 		
-		double money = a;
-		for(int i=0; i<b; i++) {
-			money = money * ((double)(100+wave[i])/100);
-		}
-		
-		System.out.println(Math.round(money)-a);
-		if(Math.round(money)-a>0.5) {
-			System.out.println("good");
-		}else if(Math.round(money)-a<-0.5) {
-			System.out.println("bad");
-		}else {
-			System.out.println("same");
+	}
+
+public static int prime_count(int number) {
+	int count = 0;
+	for(int j=2; j<number; j++) {
+		if(number%j==0) {
+			count+=1;
 		}
 	}
+	return count;
+}
 }
