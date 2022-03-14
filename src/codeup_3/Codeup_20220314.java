71dc991b4c05fec3178295011b93dbb45f630f97
jeonhaelee <jhealee0624@gmail.com>
2022-03-14 15:54:57 +0900

    시저의 암호1

---
+++ src/codeup_3/Codeup_20220314.java
@@ -0,0 +1,25 @@
+package codeup_3;
+
+import java.util.Scanner;
+
+public class Codeup_20220314 {
+	public static void main(String[] args) {
+		String answer = "";
+
+		Scanner sc = new Scanner(System.in);
+		String sentence = sc.nextLine();
+		
+		for( int i=0; i<sentence.length(); i++) {
+			if(sentence.charAt(i)==' ') {
+				answer += ' ';
+			}else {
+				if((int)sentence.charAt(i) <= 99) {
+					answer += (char)((int)sentence.charAt(i)-3 + 26);
+				}else {
+					answer += (char)((int)sentence.charAt(i)-3);
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+}
