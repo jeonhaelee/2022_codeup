19b81ed0f69e5839a9d40d78e57451bd509d663a
jeonhaelee <jhealee0624@gmail.com>
2022-03-14 16:32:33 +0900

    16진수 구구단?

--- src/codeup_3/Codeup_20220314.java
+++ src/codeup_3/Codeup_20220314.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class Codeup_20220314 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
 		
-		for(int i=1; i<=a; i++) {
-			for(int j=1; j<=b; j++) {
-				System.out.printf("%d %d\n",i,j);
-			}
+		Scanner sc = new Scanner(System.in);
+		String hex = sc.next();
+		int hexint = Integer.parseInt(hex, 16);
+		
+		for(int i=1; i<=Integer.parseInt("F", 16); i++) {
+			System.out.printf("%s*%X=%X\n",hex,i,hexint*i);
 		}
 		
 	}
