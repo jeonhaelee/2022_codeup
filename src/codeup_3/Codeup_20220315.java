1d4b9f5f0e730a705bb5ec8521beb647aa513f34
jeonhaelee <jhealee0624@gmail.com>
2022-03-15 14:04:07 +0900

    삼각형 출력하기 1

--- src/codeup_3/Codeup_20220315.java
+++ src/codeup_3/Codeup_20220315.java
@@ -10,19 +10,10 @@ public class Codeup_20220315 {
 		String star = "";
 		for(int i=0; i<n;i++) {
 			star += "*";
+			System.out.println(star);
 		}
-		String star2 = "*";
-		for(int i=1; i<n-1;i++) {
-			star2 += " ";
-		}
-		star2 += "*";
-		
-		System.out.println(star);
-		for(int i=1; i<n-1;i++) {
-			System.out.println(star2);
-		}
-		System.out.println(star);
 
 	}
 }
+
 
