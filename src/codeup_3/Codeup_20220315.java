ad64175a788cf94ecb9e59ed0a4b1bc8c159cd1b
jeonhaelee <jhealee0624@gmail.com>
2022-03-15 13:12:58 +0900

    구구단 출력하기 2

--- src/codeup_3/Codeup_20220315.java
+++ src/codeup_3/Codeup_20220315.java
@@ -5,19 +5,13 @@ import java.util.Scanner;
 public class Codeup_20220315 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int r = sc.nextInt();
-		int g = sc.nextInt();
+		int a = sc.nextInt();
 		int b = sc.nextInt();
-		
-		int count = 0;
-		for(int i=0; i<r; i++) {
-			for(int j=0; j<g; j++) {
-				for(int k=0; k<b; k++) {
-					System.out.printf("%d %d %d\n",i,j,k);
-					count += 1;
-				}
+		for (int j = a; j <= b; j++) {
+			for (int i = 1; i <= 9; i++) {
+				System.out.printf("%d*%d=%d\n", j, i, j * i);
 			}
 		}
-		System.out.println(count);
+
 	}
 }
