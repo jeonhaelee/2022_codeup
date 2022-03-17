baf36ab32f6bedbb7bfe44a4b90bc4bc3d9481d5
jeonhaelee <jhealee0624@gmail.com>
2022-03-17 22:58:31 +0900

    사각형 출력하기 4

--- src/codeup_3/Codeup_20220317.java
+++ src/codeup_3/Codeup_20220317.java
@@ -15,49 +15,28 @@ public class Codeup_20220317 {
 
 		String star_1 = "*";
 		String space = " ";
-		
+
 		System.out.println(star);
-		
-		// num이 짝수라면
-		if (num % 2 == 0) {
-			for (int i = 1; i <= num - 2; i++) {
-				String print = "";
+
+		for (int i = 1; i <= num - 2; i++) {
+			String print = "";
+			if (i == (num - 1) / 2) {
 				for (int j = 1; j <= num - 2; j++) {
-					if (j == i || j == num - 1 - i) {
+					print += star_1;
+				}
+			} else {
+				for (int j = 1; j <= num - 2; j++) {
+					if (j == i || j == num - 1 - i || j == (num-1)/2) {
 						print += star_1;
 					} else {
 						print += space;
 					}
 				}
-				System.out.println(star_1 + print + star_1);
 			}
-		} 
-		// num이 홀수라면
-		else {
-			for (int i = 1; i <= num - 2; i++) {
-				String print = "";
-				if (i == (num - 1) / 2) {
-					for (int j = 1; j <= num - 2; j++) {
-						if (j == i) {
-							print += star_1;
-						} else {
-							print += space;
-						}
-					}
-				} else {
-					for (int j = 1; j <= num - 2; j++) {
-						if (j == i || j == num - 1 - i) {
-							print += star_1;
-						} else {
-							print += space;
-						}
-					}
-				}
-				System.out.println(star_1 + print + star_1);
-			}
+			System.out.println(star_1 + print + star_1);
 		}
-		
+
 		System.out.println(star);
-		
+
 	}
 }
